9ecd27914e5d285a9b7fa41848d59aa2da8c2c65
Gabriel Rodrigues <sudsier.gabriel@gmail.com>
2023-07-30 13:31:25 -0300

    updated promocard and layout of product

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,13 +1,53 @@
 import supabase from "supabase"
+import Image from "next/image"
+import PromoCard from "src/products/components/PromoCard"
+import { useState } from "react"
 
 export default function ProductPage({ product }) {
-    console.log(product)
-    return(
-        <div>{product.name}</div>
+    const [s, setS] = useState(true)
+
+    const toggleShow = () => {
+        if(s){
+            setS(false)
+        } else {
+            setS(true)
+        }
+    }    
+
+    return (
+        <section className="product-section">
+            <article className="product">
+                <div className="product-wrap">
+                    <Image
+                        width={1000}
+                        height={300}
+                        src={`/assets/${product.slug}.png`}
+                        alt={product.name}
+                    />
+                </div>
+                <section>
+                    <header>
+                        <h3>{product.name}</h3>
+                    </header>
+                    <section onClick={toggleShow}>
+                        <div>
+                            {s ? (
+                                <p>{product.description.substring(0, 100) + '...'}</p>
+                            ) : (
+                                <p>{product.description}</p>
+                            )}
+                        </div>
+                    </section>
+                </section>
+                <section>
+                    <PromoCard />
+                </section>
+            </article>
+        </section>
     )
 }
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
     const { data: products } = await supabase.from('product').select('slug')
 
     const paths = products.map(product => ({
@@ -22,15 +62,15 @@ export async function getStaticPaths(){
     }
 }
 
-export async function getStaticProps(context){
+export async function getStaticProps(context) {
     const slug = context.params.slug
 
     const { data: product } = await supabase.from('product')
-    .select('*')
-    .eq('slug', slug)
-    .single()
+        .select('*')
+        .eq('slug', slug)
+        .single()
 
-    return{
+    return {
         props: {
             product
         }
